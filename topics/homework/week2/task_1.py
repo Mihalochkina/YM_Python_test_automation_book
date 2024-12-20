f05409e81c016f69bf57297f5d9817a3d9a38439
Yuliya Mikhalachkina <yuliya_mikhalachkina@epam.com>
2024-12-20 18:39:19 +0100

    Resolved comments

--- topics/homework/week2/task_1.py
+++ topics/homework/week2/task_1.py
@@ -1,7 +1,17 @@
-#Bubble Sort Algorithm
-#In ascending order (regular one)
-user_input = input("Введите последовательность чисел, разделенных пробелами: ")
-numbers = [int(x) for x in user_input.split()]
+# Bubble Sort Algorithm
+# In ascending order (regular one)
+def input_data():
+    user_input = input("Enter a sequence of numbers separated by spaces: ")
+    split_input = user_input.split()
+    for x in split_input:
+        try:
+            int(x)
+        except ValueError as e:
+            print("Input should contain ONLY numbers. Try once again.")
+            return input_data()
+    numbers = [int(x) for x in user_input.split()]
+    return numbers
+
 
 def bubble_sort(arr):
     n = len(arr)
@@ -13,54 +23,50 @@ def bubble_sort(arr):
 
     return arr
 
-sorted_numbers = bubble_sort(numbers)
+
+a = input_data()
+sorted_numbers = bubble_sort(a)
+
+print("Sorted list in ascending order:", sorted_numbers)
 
 
-print("Отсортированный по возрастанию список:", sorted_numbers)
-
-
-
-#In descending order
-user_input = input("Введите последовательность чисел, разделенных пробелами: ")
-numbers = [int(x) for x in user_input.split()]
-
+# In descending order
 def bubble_sort(arr):
     n = len(arr)
 
     for i in range(n):
         for j in range(0, n - i - 1):
-            if arr[j] < arr[j + 1]: # Изменено условие на <
+            if arr[j] < arr[j + 1]:  # Condition is changed to <
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
 
-sorted_numbers = bubble_sort(numbers)
+
+a = input_data()
+sorted_numbers = bubble_sort(a)
+
+print("Sorted list in descending order:", sorted_numbers)
 
 
-print("Отсортированный по убыванию список:", sorted_numbers)
-
-
-#With early stopping
-user_input = input("Введите последовательность чисел, разделенных пробелами: ")
-numbers = [int(x) for x in user_input.split()]
-
+# With early stopping
 def bubble_sort(arr):
     n = len(arr)
 
     for i in range(n):
-        swapped = False  # Добавленная переменная swapped для отслеживания замен
+        swapped = False  # Variable 'swapped' is added to track existence of swap
 
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True  # Меняем значение swapped, если была совершена замена
+                swapped = True  # Change 'swapped', if there was an exchange
 
         if not swapped:
-            break  # Выходим из цикла, если нет замен на текущем проходе
+            break  # go out of the cycle if there are no exchanges
 
     return arr
 
-sorted_numbers = bubble_sort(numbers)
 
+a = input_data()
+sorted_numbers = bubble_sort(a)
 
-print("Отсортированный список с выходом из цикла:", sorted_numbers)
+print("Sorted list with loop breakout:", sorted_numbers)
--- topics/homework/week2/task_2.py
+++ topics/homework/week2/task_2.py
@@ -1,19 +1,25 @@
-#Caching Mechanism
-number = int(input("Enter a number: "))# Prompt the user to input a number
-
+# Caching Mechanism
+# Define a cache dictionary to store previously computed squares
 cache = {}
 
+
+# Define the square function with caching mechanism
 def square(n):
-    if n in cache:  # Check if result is already in the cache
+    # Check if the result for the given n is already in the cache
+    if n in cache:
+        print("Result retrieved from cache.")
         return cache[n]
+    else:
+        # Compute the square
+        result = n * n
+        # Store the result in the cache
+        cache[n] = result
+        print("Result computed and stored in cache.")
+        return result
 
-    # If result is not in cache, calculate the square and store it in the cache
-    result = n * n
-    cache[n] = result
 
-    return result
-
-# Call the square function with the user's input
-result = square(number)
-
-print("The square of", number, "is", result)
+# Testing the square function with caching
+print(square(5))  # Result computed and stored in cache. Output: 25
+print(square(5))  # Result retrieved from cache. Output: 25
+print(square(3))  # Result computed and stored in cache. Output: 9
+print(square(3))  # Result retrieved from cache. Output: 9
